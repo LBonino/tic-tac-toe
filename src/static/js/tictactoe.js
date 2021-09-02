50a970e89a8fc6c6331717310b29a6ea82429930
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-09-02 13:31:16 -0300

    Refactor validMarks into a private variable To avoid rewriting it accidentally, a public method getValidMarks is provided instead.

--- src/static/js/tictactoe.js
+++ src/static/js/tictactoe.js
@@ -1,9 +1,10 @@
 const gameboard = (() => {
     let _squareGrid = [["", "", ""], ["", "", ""], ["", "", ""]];
-    const validMarks = ["x", "o"];
+    const _validMarks = ["x", "o"];
 
     const getState = () => _squareGrid; 
     const reset = () => {_squareGrid = [["", "", ""], ["", "", ""], ["", "", ""]]};
+    const getValidMarks = () => _validMarks;
 
     const placePiece = (mark, positionX, positionY) => {
         if (!_isValidMove(mark, positionX, positionY)) {
@@ -16,7 +17,7 @@ const gameboard = (() => {
     const _isValidMove = (mark, positionX, positionY) => {
 
         return (
-            validMarks.includes(mark.toLowerCase()) &&
+            getValidMarks().includes(mark.toLowerCase()) &&
             positionX >= 0 && positionX <= 2 &&
             positionY >= 0 && positionY <= 2 &&
             _squareGrid[positionY][positionX] === ""
@@ -27,13 +28,13 @@ const gameboard = (() => {
         reset,
         getState,
         placePiece,
-        validMarks
+        getValidMarks
     };
 })();
 
 const Player = (mark) => {
     const _mark = mark.toLowerCase()
-    if (!gameboard.validMarks.includes(_mark)) {
+    if (!gameboard.getValidMarks().includes(_mark)) {
         return console.error("Invalid mark: mark must be 'x' or 'y'");
     }
 
